429aad3227f984cea99d461c87de34b5e144aa92
Kento Kikuchi <kentokikuchi34@gmail.com>
2026-07-26 19:16:12 +0900

    ToDoアプリの基本機能を実装完了（追加、完了、削除、保存）

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+
+function TodoItem({todo, onToggle, onDelete}){
+    return(
+        <li style = {{textDecoration: todo.done ? 'line-through' : 'none'}}>
+            <input
+                type = 'checkbox'
+                checked = {todo.done}
+                onChange = {onToggle}
+            />
+            {todo.text} <button onClick = {onDelete}>削除</button>
+        </li>
+    );
+}
+
+export default function App(){
+    const [text, setText] = useState('');
+    const [todos, setTodos] = useState(() => {
+        const saved = localStorage.getItem('todos');
+        return saved ? JSON.parse(saved) : [];
+    });
+
+    useEffect(() =>{
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    function addTodo() {
+        setTodos([...todos, {text: text, done: false}]);
+        setText('');
+        //console.log(todos); // 実験的に追加
+    }
+
+    function toggleDone(index) {
+        const newTodos = todos.map((todo, i) => {
+            if (i === index) {
+                return {...todo, done: !todo.done};
+            } else {
+                return todo;
+            }
+        });
+        setTodos(newTodos);
+    }
+
+    function deleteTodo(index){
+        const newTodos = todos.filter((todo, i) => i !== index);
+        setTodos(newTodos);
+    }
+
+    return (
+        <div>
+            <h1>Hello, React ToDo App!</h1>
+            <input
+                value={text} 
+                onChange={(e) => setText(e.target.value)} 
+            /> 
+            <button onClick={addTodo}>追加</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <TodoItem 
+                        key = {index}
+                        todo = {todo} 
+                        onToggle = {() => toggleDone(index)}
+                        onDelete = {() => deleteTodo(index)}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+}
